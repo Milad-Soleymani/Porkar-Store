fd35d68844889af9b4c635c55c6882e787284a7e
Milad Coder <miladcoder1390@gmail.com>
2024-12-09 18:40:13 +0330

    Add password input

--- frontend/src/components/Login/LoginPage.jsx
+++ frontend/src/components/Login/LoginPage.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
+import AiOutlineEye from 'react-icons'
 
 const Login = () => {
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState(false);
     return (
-        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div className="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
                     Login to your account
                 </h2>
             </div>
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
                     <form className="space-y-6">
                         <div>
                             <label htmlFor="email" className='block text-sm font-medium text-gray-700'>
@@ -24,11 +26,27 @@ const Login = () => {
                                     required
                                     value={email}
                                     onChange={e => setEmail(e.target.value)}
-                                    className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                                    className='block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
                                 />
                             </div>
                         </div>
 
+                        <div>
+                            <label htmlFor="password" className='block text-sm font-medium text-gray-700'>
+                                Password
+                            </label>
+                            <div className='relative mt-1'>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    autoComplete='current-password'
+                                    required
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                                    className='block w-full px-3 py-2 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'
+                                />
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
@@ -36,4 +54,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default Login;
