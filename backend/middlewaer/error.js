07cfc7c4d73ec51c0eb1f6363e4b4e15d587bdec
Milad Coder <miladcoder1390@gmail.com>
2024-12-13 00:58:17 +0330

    semicolon

--- backend/middlewaer/error.js
+++ backend/middlewaer/error.js
@@ -6,26 +6,26 @@ module.exports = (err, req, res, next) => {
 
     //! wrong mongo id error
     if (err.name = "CastError") {
-        const message = `Resource is not found with this id ${err.path}`
-        err = new ErrorHandler(message, 400)
+        const message = `Resource is not found with this id ${err.path}`;
+        err = new ErrorHandler(message, 400);
     }
 
     //! Duplicate key error
     if (err.code === 11000) {
         const message = `Duplicate key ${Object.keys(err.keyValue)}, Entered`;
-        err = new ErrorHandler(message, 400)
+        err = new ErrorHandler(message, 400);
     }
 
     //! wrong jwt error
     if (err.name === "JsonWebTokenError") {
-        const message = 'Invalid URL, Please login again'
-        err = new ErrorHandler(message, 400)
+        const message = 'Invalid URL, Please login again';
+        err = new ErrorHandler(message, 400);
     }
 
     //! jwt expired error
     if (err.name === "TokenExpiredError") {
-        const message = 'Your URL has expired, Please login again'
-        err = new ErrorHandler(message, 400)
+        const message = 'Your URL has expired, Please login again';
+        err = new ErrorHandler(message, 400);
     }
 
     res.status(err.statusCode).json({
