16750dc6d7682c99273297faf2cace523f3b802c
Milad Coder <miladcoder1390@gmail.com>
2024-12-13 17:19:06 +0330

    Add auto upload folder creator(When isn't available)

--- backend/multer.js
+++ backend/multer.js
@@ -1,14 +1,22 @@
 const multer = require('multer');
 
+const fs = require('fs');
+const path = require('path');
+
+const dir = './uploads';
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+}
+
 const storage = multer.diskStorage({
     destination: function (req, res, cb) {
         cb(null, 'uploads/')
     },
     filename: function (req, file, cb) {
-        const uniqueSuffixe = Date.now() + "_" + Math.round.apply(Math.random * 1e9);
+        const uniqueSuffix = Date.now() + "_" + Math.round.apply(Math.random * 1e9);
         const filename = file.originalname.split('.')[0];
-        cb(null, filename + '_' + uniqueSuffixe + '.png')
+        cb(null, filename + '-' + uniqueSuffix + '.png')
     },
-})
+});
 
 exports.upload = multer({ storage: storage });
