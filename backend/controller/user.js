665923bed7061ba552851be1e1a564ee1b7b2f55
Milad Coder <miladcoder1390@gmail.com>
2024-12-13 17:17:58 +0330

    Add error finder(2)

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const { upload } = require('../multer');
 const User = require('../model/user')
 const ErrorHandler = require('../utils/ErrorHandler');
+require('express-debug')
+require('express-async-errors');
+
 
 router.post('/create-user', upload.single('file'), async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -22,7 +25,7 @@ router.post('/create-user', upload.single('file'), async (req, res, next) => {
         password: password,
         avatar: fileUrl
     };
-
+    console.log(ErrorHandler)
     console.log(user);
 })
 
