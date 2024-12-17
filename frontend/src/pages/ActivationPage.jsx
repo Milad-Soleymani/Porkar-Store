59fcae49df7788e0a06bd10c6318b0ef086847ca
Milad Coder <miladcoder1390@gmail.com>
2024-12-17 14:22:59 +0330

    Complete Setup of Activation Page

---
+++ frontend/src/pages/ActivationPage.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { server } from '../server';
+
+const ActivationPage = () => {
+    const { activation_token } = useParams();
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        if (activation_token) {
+            const activationEmail = async () => {
+                try {
+                    const res = await axios.post(`${server}/activation/`,
+                        { activation_token });
+                    console.log(res.data.message)
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+        }
+    }, [activation_token]);
+    return (
+        <div style={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            {
+                error ? (
+                    <p>Your token is expierd</p>
+                ) : (
+                    <p>Your account has been created successfully</p>
+                )
+            }
+        </div>
+    )
+}
+
+export default ActivationPage
