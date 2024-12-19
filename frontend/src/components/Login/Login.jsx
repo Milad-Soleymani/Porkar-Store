ad5cd93fac21f28bd7d4f5707bc2a8a5057b3048
Milad Coder <miladcoder1390@gmail.com>
2024-12-19 17:41:37 +0330

    Debug

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
             </div>
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
-                    <form className="space-y-6">
+                    <form className="space-y-6" onSubmit={handleSubmit}>
                         <div>
                             <label htmlFor="email" className='block text-sm font-medium text-gray-700'>
                                 Email address
