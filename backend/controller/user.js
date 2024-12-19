1c14c8174137c3d2614f9ab0d9e150485c41da2a
Milad Coder <miladcoder1390@gmail.com>
2024-12-19 21:18:49 +0330

    Setup login page

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -11,6 +11,7 @@ const sendMail = require('../utils/sendmail');
 const sendToken = require('../utils/jwtToken');
 require('express-debug')
 require('express-async-errors');
+require('dotenv').config()
 
 
 router.post('/create-user', upload.single('file'), async (req, res, next) => {
@@ -57,7 +58,7 @@ router.post('/create-user', upload.single('file'), async (req, res, next) => {
 
 // ! create activation token
 const createActivationToken = (user) => {
-    return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    return jwt.sign(user, process.env.SECRET, {
         expiresIn: "5m"
     })
 }
@@ -67,7 +68,7 @@ router.post('/activation', catchAsyncErrors(async (req, res, next) => {
     try {
         const { activation_token } = req.body;
 
-        const newUser = jwt.verify(activation_token, process.env.ACTIVATION_SECRET);
+        const newUser = jwt.verify(activation_token, process.env.SECRET);
 
         if (!newUser) {
             return next(new ErrorHandler('Invalid token', 400))
@@ -83,8 +84,8 @@ router.post('/activation', catchAsyncErrors(async (req, res, next) => {
         user = await User.create({
             name,
             email,
-            avatar,
             password,
+            avatar
         });
 
         sendToken(user, 201, res);
@@ -95,32 +96,38 @@ router.post('/activation', catchAsyncErrors(async (req, res, next) => {
 
 
 // ! login user
-router.post('/login-user', catchAsyncErrors(async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
+router.post(
+    "/login-user",
+    catchAsyncErrors(async (req, res, next) => {
+        try {
+            const {email, password} = req.body;
 
-        if (!email || !password) {
-            return next(new ErrorHandler('Please provide the all fields!', 400))
+            if (!email || !password) {
+                return next(new ErrorHandler("Please provide the all fields!", 400));
+            }
+            console.log("SECRET:", process.env.SECRET);
+            console.log("Activation Token:", activation_token);
+
+
+            const user = await User.findOne({ email }).select("+password");
+
+            if (!user) {
+                return next(new ErrorHandler("User doesn't exists!", 400));
+            }
+
+            const isPasswordValid = await user.comparePassword(password);
+
+            if (!isPasswordValid) {
+                return next(
+                    new ErrorHandler("Please provide the correct information", 400)
+                );
+            }
+
+            sendToken(user, 201, res);
+        } catch (error) {
+            return next(new ErrorHandler(error.message, 500));
         }
-
-        const user = await User.findOne({ email }).select("+password")
-
-
-        if (!user) {
-            return next(new ErrorHandler("User doesn't exists!", 401))
-        }
-    } catch (err) {
-        return next(new ErrorHandler(err.message, 500))
-    }
-
-    const isPasswordValid = await User.comparePassword(password)
-
-    if (!isPasswordValid) {
-        return next(new ErrorHandler('Incorrect password', 400))
-    }
-
-    sendToken(user, 201, res);
-}))
+    }))
 
 
 module.exports = router;
